3a9885d418f7cf6878c060a75b2f477160a7432b
gcsebas99 <gcsebas99@gmail.com>
2025-06-02 23:09:45 -0600

    Initial commit - portfolio setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css"
+
+const App = () => {
+
+  return (
+    <div className="bg-indigo-900 w-full h-screen flex items-center justify-center">
+      <p>Sebastián Gamboa's Portfolio</p>
+    </div>
+  )
+};
+
+export {
+  App
+};
